9bad073f4b07ca323a641fad04723e4b9300af74
Jason Arcala <jasonarcala@gmail.com>
2019-03-29 15:20:14 -0700

    Removed scroll listener on unmount

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
 
+const onScroll = () => {
+  let scrollSectionOffsetTop;
+  this.scrollTargetIds.map((sectionID, index) => {
+    scrollSectionOffsetTop = document.getElementById(sectionID).offsetTop - (this.headerBackground ? document.querySelector("div[data-nav='list']").scrollHeight : 0);
+
+    if (window.pageYOffset >= scrollSectionOffsetTop && window.pageYOffset < scrollSectionOffsetTop + document.getElementById(sectionID).scrollHeight) {
+      this.getNavLinkElement(sectionID).classList.add(this.activeNavClass);
+      this.clearOtherNavLinkActiveStyle(sectionID)
+    } else {
+      this.getNavLinkElement(sectionID).classList.remove(this.activeNavClass);
+    }
+
+    if (window.innerHeight + window.pageYOffset >= document.body.scrollHeight && index === this.scrollTargetIds.length - 1) {
+      this.getNavLinkElement(sectionID).classList.add(this.activeNavClass);
+      this.clearOtherNavLinkActiveStyle(sectionID);
+    }
+  });
+}
+
 class ScrollspyNav extends Component {
   constructor(props) {
     super(props);
@@ -20,24 +39,24 @@ class ScrollspyNav extends Component {
   }
 
   easeInOutQuad(current_time, start, change, duration) {
-      current_time /= duration/2;
-      if (current_time < 1) return change/2*current_time*current_time + start;
-      current_time--;
-      return -change/2 * (current_time*(current_time-2) - 1) + start;
+    current_time /= duration/2;
+    if (current_time < 1) return change/2*current_time*current_time + start;
+    current_time--;
+    return -change/2 * (current_time*(current_time-2) - 1) + start;
   };
 
   scrollTo(start, to, duration) {
     let change = to - start,
-        currentTime = 0,
-        increment = 10;
- 
-    let animateScroll = () => {        
-        currentTime += increment;
-        let val = this.easeInOutQuad(currentTime, start, change, duration);
-        window.scrollTo(0, val);
-        if(currentTime < duration) {
-            setTimeout(animateScroll, increment);
-        }
+      currentTime = 0,
+      increment = 10;
+
+    let animateScroll = () => {
+      currentTime += increment;
+      let val = this.easeInOutQuad(currentTime, start, change, duration);
+      window.scrollTo(0, val);
+      if(currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
     };
 
     animateScroll();
@@ -74,24 +93,12 @@ class ScrollspyNav extends Component {
       });
     })
 
-    window.addEventListener("scroll", () => {
-      let scrollSectionOffsetTop;
-      this.scrollTargetIds.map((sectionID, index) => {
-        scrollSectionOffsetTop = document.getElementById(sectionID).offsetTop - (this.headerBackground ? document.querySelector("div[data-nav='list']").scrollHeight : 0);
 
-        if (window.pageYOffset >= scrollSectionOffsetTop && window.pageYOffset < scrollSectionOffsetTop + document.getElementById(sectionID).scrollHeight) {
-          this.getNavLinkElement(sectionID).classList.add(this.activeNavClass);
-          this.clearOtherNavLinkActiveStyle(sectionID)
-        } else {
-          this.getNavLinkElement(sectionID).classList.remove(this.activeNavClass);
-        }
+    window.addEventListener("scroll", onScroll);
+  }
 
-        if (window.innerHeight + window.pageYOffset >= document.body.scrollHeight && index === this.scrollTargetIds.length - 1) {
-          this.getNavLinkElement(sectionID).classList.add(this.activeNavClass);
-          this.clearOtherNavLinkActiveStyle(sectionID);
-        }
-      });
-    });
+  componentWillUnmount() {
+    window.removeEventListener("scroll", onScroll);
   }
 
   clearOtherNavLinkActiveStyle(excludeSectionID) {
